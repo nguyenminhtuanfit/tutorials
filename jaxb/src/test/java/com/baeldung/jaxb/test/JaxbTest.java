96b31ba75cb0e03c57fbcb1ebb211b7def0e3f58
nguyenminhtuanfit@gmail.com <nguyenminhtuanfit@gmail.com>
2016-12-17 08:17:15 +0700

    add unit test

--- jaxb/src/test/java/com/baeldung/jaxb/test/JaxbTest.java
+++ jaxb/src/test/java/com/baeldung/jaxb/test/JaxbTest.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
-import java.util.TimeZone;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -22,15 +21,14 @@ import com.baeldung.jaxb.Book;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JaxbTest {
-    private Book book;
-    private JAXBContext context;
+    Book book;
+    JAXBContext context;
 
     @Before
     public void before() throws JAXBException {
         book = new Book();
         book.setId(1L);
         book.setName("Book1");
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         book.setDate(new Date(1481909329718L));
         context = JAXBContext.newInstance(Book.class);
     }
@@ -39,19 +37,16 @@ public class JaxbTest {
     public void marshal() throws JAXBException, IOException {
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(book, new File(this.getClass().getResource("/").getPath() + "/book.xml"));
-        File sampleBookFile = new File(this.getClass().getResource("/sample_book.xml").getFile());
-        File bookFile = new File(this.getClass().getResource("/book.xml").getFile());
-        String sampleBookXML = FileUtils.readFileToString(sampleBookFile, "UTF-8");
-        String marshallerBookXML = FileUtils.readFileToString(bookFile, "UTF-8");
+        marshaller.marshal(book, new File("./book.xml"));
+        String sampleBookXML = FileUtils.readFileToString(new File("./sample_book.xml"), "UTF-8");
+        String marshallerBookXML = FileUtils.readFileToString(new File("./book.xml"), "UTF-8");
         Assert.assertEquals(sampleBookXML, marshallerBookXML);
     }
 
     @Test
     public void unMashal() throws JAXBException, IOException {
         Unmarshaller unmarshaller = context.createUnmarshaller();
-        String bookFile = this.getClass().getResource("/book.xml").getFile();
-        Book unMarshallerbook = (Book) unmarshaller.unmarshal(new FileReader(bookFile));
+        Book unMarshallerbook = (Book) unmarshaller.unmarshal(new FileReader("./book.xml"));
         Assert.assertEquals(book, unMarshallerbook);
     }
 }
