4764c8fa503e7f90b0b3b7d2fbce0f292b6389e0
nguyenminhtuanfit@gmail.com <nguyenminhtuanfit@gmail.com>
2017-01-07 22:39:50 +0700

    test

--- spring-all/src/main/java/org/baeldung/springretry/AppConfig.java
+++ spring-all/src/main/java/org/baeldung/springretry/AppConfig.java
@@ -35,7 +35,7 @@ public class AppConfig {
         SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
         retryPolicy.setMaxAttempts(2);
         retryTemplate.setRetryPolicy(retryPolicy);
-	    ///test
+	    ///test abc
         retryTemplate.registerListener(new DefaultListenerSupport());
         return retryTemplate;
     }
